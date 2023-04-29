0cfdfcd47b331c80a2019bb41fa6e0d02a0cb556
Bojan B <83359157+bonjadev@users.noreply.github.com>
2023-04-29 05:39:02 +0200

    First commit.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Sum_of_Array_elements_in_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite zeljenu velicinu niza: ");
+            int size = int.Parse(Console.ReadLine()); //konverzija vrednosti u integer i citanje sta korisnik upise
+            int[] arr = new int[size]; //deklaracija novog niza
+
+            Console.WriteLine("Unesite elemente niza: ");
+            for (int i = 0; i < arr.Length; i++)  //arr.Length - cela duzina niza, ispisivanje niza
+            {
+                arr[i] = int.Parse(Console.ReadLine()); 
+            }
+            int result = SumOfElements(arr);
+            Console.WriteLine("sum={0}", result);
+
+        }
+
+        public static int SumOfElements(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++) //ispis niza
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+    }
+}
